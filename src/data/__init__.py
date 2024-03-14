1c92c28e949f54e33c9bed202f743d3dfbe9d344
Sviatoslav <b.svjatoslav@gmail.com>
2024-03-14 18:21:44 +0200

    Added bark

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,4 +1,4 @@
-from .tts import DummyTTSProvider, LocalTTSProvider
+from .tts import DummyTTSProvider, VitsTTSProvider, BarkTTSProvider
 from .dialogue import DummyDialogueProvider, MistralDialogueProvider, OpenaiDialogueProvider, HuggingfaceDialogueProvider
 from .theme import InMemoryThemeProvider, FileThemeProvider
 from .synth import SynthDataset, SynthExample
--- src/data/tts.py
+++ src/data/tts.py
@@ -1,5 +1,7 @@
+import random
+
 import numpy as np
-from transformers import VitsModel, AutoTokenizer
+from transformers import VitsModel, AutoTokenizer, BarkModel, BarkProcessor
 import torch
 
 
@@ -13,7 +15,7 @@ class DummyTTSProvider(BaseTTSProvider):
         return np.random.randn(16_000 * 10)
 
 
-class LocalTTSProvider(BaseTTSProvider):
+class VitsTTSProvider(BaseTTSProvider):
     def __init__(self):
         self.model = VitsModel.from_pretrained("facebook/mms-tts-eng")
         self.tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-eng")
@@ -25,3 +27,28 @@ class LocalTTSProvider(BaseTTSProvider):
             output = self.model(**inputs).waveform
 
         return output.flatten().cpu().numpy()
+
+
+class BarkTTSProvider(BaseTTSProvider):
+    def __init__(self, model_id: str = "suno/bark-small"):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = BarkModel.from_pretrained(model_id).to(self.device)
+        self.processor = BarkProcessor.from_pretrained(model_id)
+
+        self.speakers_bank = [f'v2/en_speaker_{i}' for i in range(10)]
+        self.speakers_map = {}
+
+    def generate(self, text: str, speaker: str, *args, **kwargs) -> np.array:
+        if speaker not in self.speakers_map:
+            available_speakers = list(set(self.speakers_bank) - set(self.speakers_map.values()))
+            if len(available_speakers) == 0:
+                print('No available_speakers speaker found. Reusing from bank')
+                available_speakers = self.speakers_bank
+            self.speakers_map[speaker] = random.choice(available_speakers)
+
+        speaker_preset = self.speakers_map[speaker]
+        inputs = self.processor(text, voice_preset=speaker_preset)
+        inputs = {k: v.to(self.device) for k, v in inputs.items()}
+        with torch.no_grad():
+            speech_output = self.model.generate(**inputs).flatten().cpu().numpy()
+        return speech_output
