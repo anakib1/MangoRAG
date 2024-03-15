4ef2ae8a4e29d4e5541c9be1105175e2f249a8cc
Sviatoslav <b.svjatoslav@gmail.com>
2024-03-15 21:43:39 +0200

    Fixed bark

--- src/data/tts.py
+++ src/data/tts.py
@@ -47,16 +47,37 @@ class VitsTTSProvider(BaseTTSProvider):
 
 
 class BarkTTSProvider(BaseTTSProvider):
-    def __init__(self, model_id: str = "suno/bark-small"):
+    speaker_emb = {
+        "en": [f'v2/en_speaker_{i}' for i in range(10)],
+        "ru": [f'v2/ru_speaker_{i}' for i in range(10)]
+    }
+
+    def __init__(self, model_id: str = "suno/bark-small", use_fp16: bool = False):
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.model = BarkModel.from_pretrained(model_id).to(self.device)
+        self.model = BarkModel.from_pretrained(model_id,
+                                               torch_dtype=torch.float16 if use_fp16 else None,
+                                               device_map=self.device)
         self.processor = BarkProcessor.from_pretrained(model_id)
         self.convert24to16khz = torchaudio.transforms.Resample(24_000, 16_000)
 
         self.speakers_bank = [f'v2/en_speaker_{i}' for i in range(10)]
         self.speakers_map = {}
 
-    def generate(self, text: str, speaker: str, *args, **kwargs) -> np.array:
+    def batch_processor(self, texts, voice_presets: list[str]):
+        list_dict = [self.processor(text, return_tensors="pt", voice_preset=preset) for preset, text in
+                     zip(voice_presets, texts)]
+        max_length = max(map(lambda dct: dct["attention_mask"].shape[1], list_dict))
+        for dct in list_dict:
+            for key in ["attention_mask", "input_ids"]:
+                empty = torch.zeros(1, max_length - dct[key].shape[1])
+                dct[key] = torch.cat([dct[key], empty], dim=1)
+        ans_dict = {
+            key: torch.cat([dct[key] for dct in list_dict], dim=0)
+            for key in ["attention_mask", "input_ids"]
+        }
+        return ans_dict
+
+    def generate(self, text: str, speaker: str) -> np.array:
         if speaker not in self.speakers_map:
             available_speakers = list(set(self.speakers_bank) - set(self.speakers_map.values()))
             if len(available_speakers) == 0:
@@ -71,3 +92,28 @@ class BarkTTSProvider(BaseTTSProvider):
             speech_output = self.model.generate(**inputs)
         speech_output = self.convert24to16khz(speech_output).flatten().cpu().numpy()
         return speech_output
+
+    def batch_generate(self, texts: list[str], speakers: list[int] = None, lang: str = "en") -> list[np.array]:
+        """
+        :param texts: phrases to transcribe
+        :param lang: supported languages ["ru", "en"]
+        :param speakers: voices to use for generation. for "ru" in [0 .. 9], for "en" in [0 .. 9]
+        """
+        batch_size = len(texts)
+
+        if True or speakers is None:
+            speakers_preset = [random.choice(BarkTTSProvider.speaker_emb[lang]) for i in range(batch_size)]
+        else:
+            speakers_preset = [BarkTTSProvider.speaker_emb[lang][speaker] for speaker in speakers]
+
+        inputs = self.batch_processor(texts, speakers_preset)
+        inputs = {k: v.to(self.device) for k, v in inputs.items()}
+        with torch.no_grad():
+            speech_output, lengths = self.model.generate(**inputs, return_output_lengths=True)
+
+        list_speech = []
+        for i in range(batch_size):
+            audio_array = speech_output[i][:lengths[i]]
+            audio_array = self.convert24to16khz(audio_array.cpu()).numpy()
+            list_speech.append(audio_array)
+        return list_speech
